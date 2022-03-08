357027816e9ee6e2fdaa0f53846cf9e7c75a0abd
aminyasser <aminyasser88@gmail.com>
2022-03-08 03:47:17 +0200

    solve asset problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.active_job.queue_adapter = :sidekiq
-
+  config.assets.css_compressor = nil
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
